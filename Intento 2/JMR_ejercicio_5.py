f8a2ba84bf5a3e77e3d43b5895fd6ca97ab8def8
Volibear <netallico@gmail.com>
2017-09-28 17:31:09 -0500

    Add files via upload

---
+++ Intento 2/JMR_ejercicio_5.py
@@ -0,0 +1,6 @@
+def multilinea ():
+    texto = "Hello UTNG, congratulations \n"\
+	"you are using multi-line string, in the \n"\
+	"wonderfull Python lenguage \n"
+
+    print(texto)
---
+++ Intento 2/JMR_secuencia_escape.py
@@ -0,0 +1,7 @@
+def iniciales_secuencia_escape():
+    "funcion que muestra los numeros de 1 al 10 tabulados o en escalera"
+    nums = range(0,10)
+    list (nums)
+    for n in nums:
+        print ('\t'*n, n+1)
+        
