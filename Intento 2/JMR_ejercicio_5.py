3e30845598f731329ff199d979debcac814f2b7e
Volibear <netallico@gmail.com>
2017-09-28 17:39:13 -0500

    Update JMR_ejercicio_5.py

--- Intento 2/JMR_ejercicio_5.py
+++ Intento 2/JMR_ejercicio_5.py
@@ -1,4 +1,5 @@
 def multilinea ():
+    "Esta funcion escribe un texto en multilineas"
     texto = "Hello UTNG, congratulations \n"\
 	"you are using multi-line string, in the \n"\
 	"wonderfull Python lenguage \n"
